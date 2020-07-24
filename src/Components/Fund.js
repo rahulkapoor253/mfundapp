8ea8a57b20678d1b79d9fd2ca30b05b9658e51cf
Rahul Kapoor <rahul253801@gmail.com>
2020-07-24 21:05:16 +0530

    fund details api implemented and plotted on cavajs stock chart

--- src/Components/Fund.js
+++ src/Components/Fund.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Fund.css';
+import CanvaJSReact from '../canvasjs.react';
+import axios from 'axios';
 
 class Fund extends React.Component {
 
@@ -7,14 +9,64 @@ constructor(props) {
     super(props);
 
     this.state = {
-        schemeCode : ''
+        schemeCode : props.location.state.schemeCode,
+        dataPoints : []
     }
 }
 
+componentDidMount() {
+    var chart = this.chart;
+    //call api with schemecode
+    axios.get(`https://api.mfapi.in/mf/${this.state.schemeCode}`)
+    .then(response => {
+        const fundDetails = response.data;
+        console.log(fundDetails);
+        const val = parseInt(fundDetails.data.length / 90);
+        for (var i = 0; i < fundDetails.data.length; i=i+val) {
+            this.state.dataPoints.push({
+                x: new Date(fundDetails.data[i].date),
+                y: parseFloat(fundDetails.data[i].nav)
+            });
+        }
+        chart.render();
+    })
+    .catch(error => {
+      console.log(error);
+      alert("Unable to fetch fund details. Please Try again...");
+    })
+
+}
+
 render() {
+
+    const options = {
+        theme: "light2",
+        title: {
+            text: "NAV Chart"
+        },
+        axisY: {
+            title: "NAV Unit Price",
+            includeZero: false,
+            crosshair: {
+                enabled: true,
+                snapToDataPoint: true
+            }
+        },
+        data: [{
+            type: "scatter",
+            xValueFormatString: "MMM YYYY",
+            yValueFormatString: "###0.00",
+            dataPoints: this.state.dataPoints
+        }]
+    }
+
     return (
-        <div>
-            {this.props.location.state.schemeCode}
+        <div className="Fund">
+             <header className="Fund-header">
+                <CanvaJSReact.CanvasJSChart options = {options} 
+                    onRef={ref => this.chart = ref}
+                />
+            </header>
         </div>
     )
 }
